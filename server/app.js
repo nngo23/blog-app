bea7b0631cf0016e9835392032c1c215332b7830
Ngo Ngoc Sang <ngocsang26889@gmail.com>
2026-01-23 01:48:47 +0200

    fix tag release

--- server/app.js
+++ server/app.js
@@ -43,7 +43,7 @@ if (process.env.NODE_ENV === "test") {
 if (process.env.NODE_ENV !== "test") {
   app.use(express.static(path.join(__dirname, "public")));
 
-  app.get("/*", (req, res) => {
+  app.use("*", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "index.html"));
   });
 }
