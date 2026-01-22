441ac5817772e2413bce00b0075ba16ab7d9b93a
Ngo Ngoc Sang <ngocsang26889@gmail.com>
2026-01-22 21:31:23 +0200

    update baseUrl, env

--- client/src/services/login.js
+++ client/src/services/login.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
-const baseUrl = import.meta.env.VITE_BACKEND_URL;
+const baseUrl =
+  import.meta.env?.VITE_BACKEND_URL ||
+  process.env.VITE_BACKEND_URL ||
+  "http://localhost:3004";
 
 const login = async (credentials) => {
   const response = await axios.post(`${baseUrl}/api/login`, credentials);
